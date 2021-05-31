ffbbe9b454dff366261d97c7558fcd52f5c48371
Franco Melandri <franco.melandri@gmail.com>
2021-05-31 10:40:56 +0200

    introducing RAFT layer over raft-core library

---
+++ src/raft/Node/Node.cs
@@ -0,0 +1,15 @@
+﻿using RaftCore.Cluster;
+using RaftCore.Models;
+
+namespace Raft.Node
+{
+    public class Node : INode
+    {
+        public int Id { get; private set; }
+
+        public Node(NodeConfiguration nodeConfiguration)
+        {
+            Id = nodeConfiguration.Id;
+        }
+    }
+}
---
+++ test/raft/Node/NodeTests.cs
@@ -0,0 +1,28 @@
+﻿using FluentAssertions;
+using NUnit.Framework;
+using Raft.Node;
+using RaftCore.Models;
+
+namespace RaftCoreTest
+{
+    [TestFixture]
+    public class NodeTests
+    {
+        private Node _sut;
+        private NodeConfiguration  _nodeConfiguration;
+
+        [SetUp]
+        public void SetUp()
+        {
+            _nodeConfiguration = new NodeConfiguration
+            {
+                Id = 1
+            };
+            _sut = new Node(_nodeConfiguration);
+        }
+
+        [Test]
+        public void Node_HaCorrectId()
+            => _sut.Id.Should().Be(1);
+    }
+}
