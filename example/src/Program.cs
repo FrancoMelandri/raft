99ea8307085bc075fba10f0fa621184dc64ea629
Franco Melandri <franco.melandri@gmail.com>
2021-05-31 12:53:46 +0200

    introducing example application

---
+++ example/src/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Hosting;
+using System.Diagnostics.CodeAnalysis;
+using Microsoft.Extensions.Logging;
+using TinyFp.Extensions;
+
+namespace RaftApplication
+{
+    [ExcludeFromCodeCoverage]
+    public static class Program
+    {
+        public static void Main()
+            => Host.CreateDefaultBuilder(Array.Empty<string>())
+                .ConfigureLogging(logging => logging
+                                                .Tee(_ => _.ClearProviders())
+                                                .Tee(_ => _.AddConsole()))
+                .ConfigureWebHostDefaults(webBuilder =>
+                {
+                    webBuilder.UseStartup<Startup>();
+                })
+                .Build()
+                .Run();
+    }
+}
---
+++ example/src/Startup.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using System.Diagnostics.CodeAnalysis;
+using TinyFp.Extensions;
+using static TinyFp.Extensions.Functional;
+
+namespace RaftApplication
+{
+    [ExcludeFromCodeCoverage]
+    public class Startup
+    {
+        protected IConfiguration Configuration { get; }
+
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public virtual void ConfigureServices(IServiceCollection services)
+            => services
+                .Tee(_ => _.AddControllers());
+
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+            => app
+                .UseRouting()
+                .UseEndpoints(_ => _.MapControllers());
+    }
+}
