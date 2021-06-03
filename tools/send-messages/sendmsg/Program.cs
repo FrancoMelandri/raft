ee742e605e93011587a692c7a25afc08ef0c1cd2
Franco Melandri <franco.melandri@gmail.com>
2021-06-03 11:49:18 +0200

    updating tool to send messages

--- tools/send-messages/sendmsg/Program.cs
+++ tools/send-messages/sendmsg/Program.cs
@@ -7,16 +7,16 @@ namespace sendmsg
         static void Main(string[] args)
         {
             System.Threading.Thread.Sleep(5000);
-            SendMessage("{\"Type\":1}");
+            SendMessage(1, "{\"Type\":1}");
 
             System.Threading.Thread.Sleep(5000);
-            SendMessage("{\"Type\":2}");
+            SendMessage(2, "{\"Type\":2}");
 
             System.Threading.Thread.Sleep(5000);
-            SendMessage("{\"Type\":3}");
+            SendMessage(3, "{\"Type\":3}");
         }
 
-        private static void SendMessage(string message)
+        private static void SendMessage(int type, string message)
         {
             TcpClient client = new();
             client.Connect("localhost", 3000);
@@ -24,6 +24,9 @@ namespace sendmsg
             var header = message.Length.ToString().PadLeft(16, ' ');
             var buffer = System.Text.Encoding.UTF8.GetBytes(header);
             client.GetStream().Write(buffer, 0, buffer.Length);
+
+            buffer = System.Text.Encoding.UTF8.GetBytes(type.ToString());
+            client.GetStream().Write(buffer, 0, 1);
 
             buffer = System.Text.Encoding.UTF8.GetBytes(message);
             client.GetStream().Write(buffer, 0, buffer.Length);
