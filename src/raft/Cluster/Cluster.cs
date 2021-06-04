07e306969648b88539e5568d72518fe79b0ff87c
Franco Melandri <franco.melandri@gmail.com>
2021-06-04 10:32:28 +0200

    enabling cluster sending message using cluster nodes

--- src/raft/Cluster/Cluster.cs
+++ src/raft/Cluster/Cluster.cs
@@ -1,6 +1,8 @@
 ﻿using RaftCore.Cluster;
 using RaftCore.Models;
+using System.Linq;
 using TinyFp;
+using TinyFp.Extensions;
 
 namespace Raft.Cluster
 {
@@ -14,9 +16,14 @@ namespace Raft.Cluster
         }
 
         public Unit SendBroadcastMessage(Message message)
-            => Unit.Default;        
+            => Unit.Default
+                .Tee(_ => Nodes.ForEach(node => node.SendMessage(message)));
 
         public Unit SendMessage(int nodeId, Message message)
-            => Unit.Default;
+            => Unit.Default
+                .Tee(_ => Nodes
+                            .FirstOrDefault(node => node.Id == nodeId)
+                            .ToOption()
+                            .OnSome(node => node.SendMessage(message)));
     }
 }
